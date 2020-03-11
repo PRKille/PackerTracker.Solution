a4272137e94a6a701ab5914f2cdbad2ccfc2f366
Student <example@example.com>
2020-03-11 13:26:22 -0700

    refactored

--- PackerTracker/Controllers/ItemsController.cs
+++ PackerTracker/Controllers/ItemsController.cs
@@ -31,17 +31,21 @@ namespace PackerTracker.Controllers
     }
 
     [HttpPost("/items/{id}")]
-    public ActionResult Update(int id, string value)
+    public ActionResult Update(int id, string packed, string purchased)
     {
       Item purchasedItem = Item.GetAll()[id-1];
-      if (value == "purchased")
+      if (purchased == "true")
       {
         purchasedItem.MarkPurchased();
         return RedirectToAction("Items");
       }
-      else
+      else if (packed == "true")
       {
         purchasedItem.MarkPacked();
+        return RedirectToAction("Items");
+      }
+      else
+      {
         return RedirectToAction("Items");
       }
     }
